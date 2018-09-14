36202ec421ee7e135fcc451d594613b6799f3b58
CodeWinger <mathieu.boucher5@gmail.com>
2018-09-14 00:17:26 -0400

    CodeCoverageTest

--- src/main/java/some/project/testing/App.java
+++ src/main/java/some/project/testing/App.java
@@ -6,6 +6,20 @@ package some.project.testing;
  */
 public class App 
 {
+	private final int x;
+	
+	public App(int s) {
+		x = s;
+	}
+	
+	public void get() {
+		if (x < 0) {
+	        System.out.println( "x<0" );
+		} else {
+	        System.out.println( "x>0" );
+		}
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- src/test/java/some/project/testing/AppTest.java
+++ src/test/java/some/project/testing/AppTest.java
@@ -1,38 +1,23 @@
 package some.project.testing;
 
-import junit.framework.Test;
+import org.junit.Test;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
     extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+{   
+    @Test
+    public void xLessThan0Test() {
+    	new App(-5).get();
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    
+    @Test
+    public void xGreaterThan0Test() {
+    	new App(5).get();
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    
 }
